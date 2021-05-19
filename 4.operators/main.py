408b10c830687466cf763508381df1b3bebe04bf
jhondev-code <jhondev.code@gmail.com>
2021-05-19 18:19:00 -0500

    Operators

---
+++ 4.operators/main.py
@@ -0,0 +1,24 @@
+a = 10
+b = 5
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a ** b)
+print(a % b)
+
+print(True and True)
+print(True and False)
+print(False and True)
+print(False and False)
+
+print(True or True)
+print(True or False)
+print(False or True)
+print(False or False)
+
+print(not True)
+print(not True)
+print(not False)
+print(not False)
