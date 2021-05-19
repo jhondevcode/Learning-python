0dab8c01186f216e48e75dae963e167522f9e59e
jhondev-code <jhondev.code@gmail.com>
2021-05-19 18:43:07 -0500

    Dictionaries

---
+++ 6.data_structures/dictionaries.py
@@ -0,0 +1,22 @@
+tel = {'Steve': 409533468, 'Ryan': 413157349}
+
+print(tel)
+
+print(tel['Ryan'])
+
+tel['Maria'] = 982827964
+
+print(tel)
+print(tel['Maria'])
+
+print(sorted(tel))
+
+print("Steve" in tel)
+print("Bryan" in tel)
+
+fruits = dict([("Orange", 1.25), ("Pear", 0.32), ("Banana", 0.16), ("Apple", 1.062)])
+
+print(fruits)
+
+print("Orange" in fruits)
+print("Apple" not in fruits)
