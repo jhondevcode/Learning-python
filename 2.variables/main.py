8bd083827bd9aa65102464e290bbcf240391456e
jhondev-code <jhondev.code@gmail.com>
2021-05-19 18:03:46 -0500

    Variables

---
+++ 2.variables/main.py
@@ -0,0 +1,20 @@
+# Integers
+vari0 = 25328612
+vari1 = 1865322
+bitewise = 0x0021
+print(bitewise)
+
+# Decimals
+varf0 = 25142.2646552525
+varf1 = 11.2546422562666
+pi = 3.141592653589793238462
+
+print(pi)
+
+# Strings
+lang = "Python"
+repo = "Github"
+native_os = "Linux"
+editor = "Atom"
+
+print(lang, repo, native_os, editor)
